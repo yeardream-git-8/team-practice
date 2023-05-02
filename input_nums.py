ab2fa2644e434546837f5e4047a67cf2d3083fdd
yn9634 <sanghun9634@gmail.com>
2023-05-02 15:14:44 +0900

    feat: Add input 4 nums
    0~9 range input 4 nums and exclude duplicate values

--- input_nums.py
+++ input_nums.py
@@ -7,9 +7,11 @@ def input_nums():
         a = int(input('숫자를 입력하세요'))
         
         if a >= 0 and a <= 9:
-            num_list.append(a)
-            print(i)
-            i += 1
+            if a not in num_list:
+                num_list.append(a)
+                i += 1
+            else:
+                print('중복된 값입니다 다시 입력해주세요')
         else:
             print('0~9까지의 숫자를 입력해주세요')
     return num_list
