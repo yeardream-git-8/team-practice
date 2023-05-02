3470bbf405df2e9fe9905f6f3b45b4ac0e75ade6
yn9634 <sanghun9634@gmail.com>
2023-05-02 14:30:22 +0900

    feat: Add exclude duplicate values

--- rand_nums.py
+++ rand_nums.py
@@ -2,11 +2,14 @@ import random
 
 def rand_nums():
     num_list = []
+
     for i in range(4):
-        num_list.append(random.randint(0,9))
-    print(num_list)
+        num = random.randint(0,9)
+        while num in num_list:
+            num = random.randint(0,9)
+        num_list.append(num)
 
     return num_list
 
 
-rand_nums()
+print(rand_nums())
