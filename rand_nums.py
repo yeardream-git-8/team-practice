13ef63d53b8798498df1a3a72ea083f013e6025a
yn9634 <sanghun9634@gmail.com>
2023-05-02 14:27:09 +0900

    fix: output range 1 to 9 -> 0 to 9

--- rand_nums.py
+++ rand_nums.py
@@ -3,7 +3,7 @@ import random
 def rand_nums():
     num_list = []
     for i in range(4):
-        num_list.append(random.randint(1,9))
+        num_list.append(random.randint(0,9))
     print(num_list)
 
     return num_list
